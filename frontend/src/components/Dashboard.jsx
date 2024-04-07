c3e24cd969c5a0e30fabe3f502d3fd5d8184f5fe
Varun Reddy Rajireddy <itsvarun2798@gmail.com>
2024-04-07 12:31:29 -0500

    Added Add and Delete task functionalities in the frontend

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -6,6 +6,7 @@ import axios from "axios";
 const Dashboard = () => {
   const navigate = useNavigate();
   const { user, logout } = useContext(AuthContext);
+  const [createTask, setCreateTask] = useState("");
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -37,9 +38,57 @@ const Dashboard = () => {
     fetchTasks();
   }, [user]); // Only trigger useEffect when user changes
 
-  const handleDelete = () => {
+  const handleLogout = () => {
     logout();
     navigate("/");
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      if (!user || !user.token) {
+        console.log("User or token is missing.");
+        return;
+      }
+      const token = user.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      await axios.delete(`http://localhost:5000/api/tasks/${id}`, config);
+      const remainingTasks = tasks.filter((task) => {
+        return task._id !== id;
+      });
+      console.log(remainingTasks);
+      setTasks(remainingTasks);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  const addTask = async () => {
+    try {
+      if (!user || !user.token) {
+        console.log("User or token is missing.");
+        return;
+      }
+      const token = user.token;
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.post(
+        `http://localhost:5000/api/tasks/add`,
+        { title: createTask },
+        config
+      );
+      const newTask = response.data;
+      setTasks([...tasks, newTask]);
+      setCreateTask("");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!user) {
@@ -48,11 +97,22 @@ const Dashboard = () => {
 
   return (
     <div>
-      Dashboard
-      <button onClick={handleDelete}>Logout</button>
-      {tasks.map(({ title }) => (
-        <div key={title}>{title}</div>
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={createTask}
+        onChange={(e) => {
+          setCreateTask(e.target.value);
+        }}
+      />
+      <button onClick={addTask}>Add</button>
+      {tasks.map((task) => (
+        <div key={task._id}>
+          {task.title}{" "}
+          <button onClick={() => handleDelete(task._id)}>Del</button>
+        </div>
       ))}
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
